30cc647e05315182cad82431bd1a2321251869be
Polina <991201@bk.ru>
2021-06-09 23:43:14 +0300

    create smooth scrolling through links and up element

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,3 +16,20 @@ const percent = document.querySelectorAll('.skills__rating-percent'),
 percent.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+// Smooth scroll and pageup
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+    $('.pageup').fadeIn();
+    } else {
+    $('.pageup').fadeOut();
+    }
+});
+
+$("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+new WOW().init();
